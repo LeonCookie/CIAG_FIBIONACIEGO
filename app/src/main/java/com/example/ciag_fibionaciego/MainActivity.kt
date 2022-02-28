da07b8afae5d0027d531c9d7b509237ae92888cf
leoncookie <leongdynia1105@gmail.com>
2022-02-28 15:19:51 +0100

    lateinit var

--- app/src/main/java/com/example/ciag_fibionaciego/MainActivity.kt
+++ app/src/main/java/com/example/ciag_fibionaciego/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.ciag_fibionaciego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var numberText:EditText
+        lateinit var button:Button
+        lateinit var progressBar:ProgressBar
+
+        numberText = findViewById<EditText>(R.id.NumberIn)
+        button = findViewById<Button>(R.id.button)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        var X:Int=0;//Numer z edit text bar
+
+        button.setOnClickListener{
+
+        }
+
     }
+
+
+
+
+
 }
